6ec5c3f9e69892a0c85e324a4ce36e150d62f4b4
BrendaDenhoff <brenda.denhoff@estudiantes.unahur.edu.ar>
2022-06-26 16:02:07 -0300

    se elimina todo lo relacionado con la tabla alumno

--- api/app.js
+++ api/app.js
@@ -5,8 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var carrerasRouter = require('./routes/carreras');
 var materiasRouter = require('./routes/materias');
-var alumnosRouter = require('./routes/alumno')
-
 
 var app = express();
 
@@ -23,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/car', carrerasRouter);
 app.use('/mat', materiasRouter);
-app.use('/alu', alumnosRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
